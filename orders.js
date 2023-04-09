e7a16a7154d6a1f054beae27c5d7bdfa9db0b136
kahafMd <103684304+kahafMd@users.noreply.github.com>
2023-04-10 00:39:23 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '36632',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '74632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '99021',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '34897',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '78532',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
